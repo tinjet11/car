bb6b3bec47014a78f0ff4cbec7ab93a656c2ee58
tinjet_11 <tinjet1016@gmail.com>
2023-06-12 11:43:50 +0800

    search bar small bugs fix

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -29,7 +29,7 @@ const SearchBar = () => {
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        if (manufacturer === '' || model === '') {
+        if (manufacturer === '' &&  model === '') {
             return alert('Please fill in the search bar');
         }
 

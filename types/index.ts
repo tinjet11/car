47fd986a047b9fbb64cf976623c9dc80bf360b08
tinjet_11 <tinjet1016@gmail.com>
2023-06-12 11:38:51 +0800

    testing

---
+++ types/index.ts
@@ -0,0 +1,62 @@
+import { MouseEventHandler } from "react";
+
+
+export interface CustomButtonProps {
+  isDisabled?: boolean;
+  btnType?: "button" | "submit" | "reset";
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManuFacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
+
+
+
+export interface FilterProps {
+  manufacturer: string,
+  year: number,
+  fuel: string,
+  limit: number,
+  model: string,
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
